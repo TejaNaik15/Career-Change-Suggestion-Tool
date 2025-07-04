a6fd54ee039893d1a84c11d9de938369e8f91567
TEJANAIK <134024083+TejaNaik15@users.noreply.github.com>
2025-07-04 12:56:33 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: "class", // Enable dark mode based on 'dark' class
+  darkMode: "class", 
   theme: {
     extend: {
       animation: {
